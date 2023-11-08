9e2cf25e9f325266e0656c6ad1dcc6434d760f0c
Ratha <ratha.pech@gmail.com>
2023-11-08 13:10:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Somothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("🥣 Breakfast menu")
+streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🐔 Kale, Spinach & Rocket Somothie")
+streamlit.text("🥑🍞 Hard-Boiled Free-Range Egg")
+
+streamlit.header("🍌🥭 Build Your Own Menu 🥝🍇")
